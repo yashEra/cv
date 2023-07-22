968a6f2bdf5c91d86eceb5c501a83103dbd29158
Yasith Eranda Yapa Amarasekara <ya794120@gmail.com>
2023-07-22 12:23:34 +0530

    Update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a82d9f085bef7441"; }
+/******/ 		__webpack_require__.h = function() { return "2fbb2fc711c4d25c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
